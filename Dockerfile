a66bf3d66508c7c66c34ba550a2755895781283b
Joris De Winne <joris.dewinne@gmail.com>
2016-11-03 15:12:12 -0700

    Adding copyDownloadResources

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM xebialabs/xl-docker-demo-xld:v5.5.5.1
 
 MAINTAINER Joris De Winne <jdewinne@xebialabs.com>
-ENV REFRESHED_AT 2016-10-17
+ENV REFRESHED_AT 2016-11-03
 
 RUN apk --update add openjdk8 bash libstdc++
 ADD resources/command.sh /opt/xld/command.sh
--- resources/command.sh
+++ resources/command.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env sh
 
+cd /data/
+./gradlew clean copyDownloadResources
+
+# Copy downloads into 'plugins'
+if [ -d "/data/build/downloads/plugins" ]; then
+    cp /data/build/downloads/plugins/* /opt/xld/server/plugins
+fi
+
 # Compile plugin
 cd /data/
 ./gradlew clean test assemble -PxlDeployHome=/opt/xld/server
