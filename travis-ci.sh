d1705bc06d4adb2283b3b22730d0056c33c281a1
Joris De Winne <joris.dewinne@gmail.com>
2018-07-17 13:24:28 -0700

    Enable push

--- travis-ci.sh
+++ travis-ci.sh
@@ -9,5 +9,5 @@ image_name="xebialabsunsupported/xld_dev_compile"
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 while read tag ; do
     docker build -t $image_name:$tag --build-arg xld_tag=$tag .
-    # docker push $image_name:$tag
+    docker push $image_name:$tag
 done < <(comm -23 <(sort /tmp/xld) <(sort /tmp/xl))
