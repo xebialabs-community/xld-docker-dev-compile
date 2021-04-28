97c7b96dbd35b21391095bd7e5cf920057373b44
Rick Broker <rick@rancher.inhouse-broker.org>
2021-04-28 16:09:31 -0400

    Rebuild all images

--- cleanup.sh
+++ cleanup.sh
@@ -1,30 +1,16 @@
 #!/bin/bash
 
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-TOKEN=$(curl -s -H "Content-Type: application/json" -X POST -d "
-{
-\"username\": \"$DOCKER_USERNAME\",
-\"password\": \"$DOCKER_PASSWORD\"
-}
-" https://hub.docker.com/v2/users/login/ | jq -r .token)
-
 http --follow https://gist.githubusercontent.com/jdewinne/3f13494858fad8b6b2b88ebd3439f1ea/raw/dce2f06fea89a6038acd6944c516c8be9347c48d/dockertags > ./dockertags
 chmod +x dockertags
 
 image_name="xebialabsunsupported/xld_dev_compile"
 ./dockertags -i xebialabs/xl-deploy -e "centos$" > /tmp/xld
 ./dockertags -i $image_name > /tmp/xl
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 while read tag ; do
-    docker build -t $image_name:$tag --build-arg xld_tag=$tag .
     echo "####################################################################"
-    echo "####        $image_name:$tag built"
-    docker push $image_name:$tag
-    echo "####        $image_name:$tag pushed"
-    echo "####----------------------------------------------------------------"
     docker rmi $image_name:$tag
     echo "####        $image_name:$tag removed"
     docker rmi xebialabs/xl-deploy:$tag
     echo "####        $image_name:$tag removed"
     echo "####################################################################"
-done < <(comm -23 <(sort /tmp/xld) <(sort /tmp/xl))
+done < <(sort /tmp/xld)
